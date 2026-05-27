6cf4aae82c9fe9e303b82dfa1968d859b99306dd
CabeloSG <leandrorecife@hotmail.com>
2026-05-26 21:09:10 -0400

    fix: render docker build

--- biblioteca.anandamoyi/src/main/java/br/com/biblioteca/anandamoyi/infra/config/SwaggerConfig.java
+++ biblioteca.anandamoyi/src/main/java/br/com/biblioteca/anandamoyi/infra/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package br.com.biblioteca.anandamoyi.infra.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -10,42 +12,66 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
     @Bean
     public OpenAPI customOpenAPI() {
 
-        final String securitySchemeName = "bearerAuth";
-
         return new OpenAPI()
 
-                .info(new Info()
-                        .title("Biblioteca Mestre Ramatis API")
-                        .version("1.0")
-                        .description("""
-                                API REST para gerenciamento de biblioteca.
-
-                                Funcionalidades:
-                                - autenticação JWT
-                                - cadastro de usuários
-                                - cadastro de livros
-                                - empréstimos
-                                - devoluções
-                                - multas automáticas
-                                - controle de atraso
-                                """))
-
+                // JWT
                 .addSecurityItem(
                         new SecurityRequirement()
-                                .addList(securitySchemeName)
+                                .addList(SECURITY_SCHEME_NAME)
                 )
 
                 .schemaRequirement(
-                        securitySchemeName,
+                        SECURITY_SCHEME_NAME,
 
                         new SecurityScheme()
-                                .name(securitySchemeName)
+                                .name(SECURITY_SCHEME_NAME)
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")
+                                .description("Insira o token JWT no formato: Bearer {token}")
+                )
+
+                // INFO API
+                .info(
+                        new Info()
+                                .title("Biblioteca Mestre Ramatis API")
+                                .version("1.0.0")
+                                .description("""
+                                        API REST para gerenciamento de biblioteca.
+
+                                        Funcionalidades:
+                                        - autenticação JWT
+                                        - cadastro de usuários
+                                        - cadastro de livros
+                                        - empréstimos
+                                        - devoluções
+                                        - multas automáticas
+                                        - controle de atraso
+                                        - controle de permissões
+
+                                        Projeto desenvolvido com:
+                                        - Spring Boot
+                                        - Spring Security
+                                        - JWT
+                                        - PostgreSQL
+                                        - Docker
+                                        - JaCoCo
+                                        - Swagger/OpenAPI
+                                        """)
+                                .contact(
+                                        new Contact()
+                                                .name("Leandro Gonçalves")
+                                                .url("https://github.com/CabeloSG")
+                                )
+                                .license(
+                                        new License()
+                                                .name("Projeto Educacional")
+                                )
                 );
     }
 }

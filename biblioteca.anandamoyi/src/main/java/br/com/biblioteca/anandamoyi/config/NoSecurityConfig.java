9f2bc3503c9af9da41823c56f50c22de4ea2394a
CabeloSG <leandrorecife@hotmail.com>
2026-05-27 13:21:17 -0400

    fix cors security options

--- biblioteca.anandamoyi/src/main/java/br/com/biblioteca/anandamoyi/config/NoSecurityConfig.java
+++ biblioteca.anandamoyi/src/main/java/br/com/biblioteca/anandamoyi/config/NoSecurityConfig.java
@@ -1,20 +1,21 @@
 package br.com.biblioteca.anandamoyi.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
 @Profile("test")
 public class NoSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         return http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll()
+                )
                 .build();
     }
 }
--- biblioteca.anandamoyi/src/main/java/br/com/biblioteca/anandamoyi/infra/security/SecurityConfig.java
+++ biblioteca.anandamoyi/src/main/java/br/com/biblioteca/anandamoyi/infra/security/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
                 // DESABILITA CSRF
                 .csrf(AbstractHttpConfigurer::disable)
 
+                .cors(cors -> {})
+
                 // JWT = STATELESS
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -49,6 +51,8 @@ public class SecurityConfig {
 
                 // REGRAS
                 .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
                         // PUBLICO
                         .requestMatchers(

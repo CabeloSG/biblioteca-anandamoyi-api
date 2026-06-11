b36d59a0b64e95965c1a845af0acc59057fab139
CabeloSG <leandrorecife@hotmail.com>
2026-06-11 14:42:49 -0400

    fix: libera endpoint teste-arquivo

--- biblioteca.anandamoyi/src/main/java/br/com/biblioteca/anandamoyi/infra/security/SecurityConfig.java
+++ biblioteca.anandamoyi/src/main/java/br/com/biblioteca/anandamoyi/infra/security/SecurityConfig.java
@@ -60,7 +60,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
                                 "/webjars/**",
-                                "/uploads/**"
+                                "/uploads/**",
+                                "/livros/teste-arquivo"
                         ).permitAll()
 
                         // CRIAR USUARIO

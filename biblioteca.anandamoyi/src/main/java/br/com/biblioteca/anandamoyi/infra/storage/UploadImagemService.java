9de1cb1b4c8e163593907d8be136ed5ffae5f10a
CabeloSG <leandrorecife@hotmail.com>
2026-06-18 09:48:57 -0400

    feat: integra cloudinary

--- biblioteca.anandamoyi/src/main/java/br/com/biblioteca/anandamoyi/infra/storage/UploadImagemService.java
+++ biblioteca.anandamoyi/src/main/java/br/com/biblioteca/anandamoyi/infra/storage/UploadImagemService.java
@@ -1,58 +1,38 @@
 package br.com.biblioteca.anandamoyi.infra.storage;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.UUID;
+import java.util.Map;
 
 @Service
 public class UploadImagemService {
 
-    private static final String PASTA_UPLOAD = "uploads/livros";
+    private final Cloudinary cloudinary;
+
+    public UploadImagemService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
 
     public String salvar(MultipartFile arquivo) {
 
         try {
 
-            if (arquivo.isEmpty()) {
-                throw new IllegalArgumentException("Arquivo vazio");
-            }
+            Map<?, ?> resultado = cloudinary.uploader().upload(
+                    arquivo.getBytes(),
+                    ObjectUtils.asMap(
+                            "folder", "biblioteca/livros"
+                    )
+            );
 
-            String nomeOriginal = arquivo.getOriginalFilename();
+            return resultado.get("secure_url").toString();
 
-            String extensao = "";
+        } catch (Exception e) {
 
-            if (nomeOriginal != null && nomeOriginal.contains(".")) {
-                extensao =
-                        nomeOriginal.substring(
-                                nomeOriginal.lastIndexOf(".")
-                        );
-            }
-
-            String nomeArquivo =
-                    UUID.randomUUID() + extensao;
-
-            Path pasta =
-                    Paths.get(PASTA_UPLOAD);
-
-            if (!Files.exists(pasta)) {
-                Files.createDirectories(pasta);
-            }
-
-            Path destino =
-                    pasta.resolve(nomeArquivo);
-
-            arquivo.transferTo(destino);
-
-            return "/uploads/livros/" + nomeArquivo;
-
-        } catch (IOException e) {
             throw new RuntimeException(
-                    "Erro ao salvar imagem",
+                    "Erro ao enviar imagem para Cloudinary",
                     e
             );
         }
